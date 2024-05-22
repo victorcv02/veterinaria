8726d354f48b9eb98cb3934392781b48af5a11ea
victorcv02 <victorcv76534742@gmail.com>
2024-05-21 23:02:16 -0500

    Delete Veterinaria_avancefinal directory

--- Veterinaria_avancefinal/src/CapaDeInfraestructura/Parametros.java
+++
@@ -1,9 +0,0 @@
-package CapaDeInfraestructura;
-
-
-public interface Parametros {    
-    String DRIVER="com.mysql.cj.jdbc.Driver";
-    String RUTA="jdbc:mysql://localhost:3306/bdveterinaria";
-    String USUARIO="root";
-    String CLAVE="";    
-}
